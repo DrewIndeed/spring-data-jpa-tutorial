148a9e01a5e11fadad6bb0de5be2c522d19cdab5
Andrew Le <s3820098@rmit.edu.vn>
2022-02-24 09:05:06 +0700

    2.  Custom names annotations

--- src/main/java/com/drewindeed/spring/data/jpa/tutorial/entity/Student.java
+++ src/main/java/com/drewindeed/spring/data/jpa/tutorial/entity/Student.java
@@ -2,8 +2,10 @@ package com.drewindeed.spring.data.jpa.tutorial.entity;
 
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Getter
@@ -12,11 +14,14 @@ import javax.persistence.Id;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(name = "tbl_student")
 public class Student {
     @Id
     private Long studentId;
     private String firstName;
     private String lastName;
+
+    @Column(name = "email_address")
     private String emailId;
     private String guardianName;
     private String guardianEmail;
