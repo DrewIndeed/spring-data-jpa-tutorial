60f444a994b145caa097b66fb230b183ef631c9b
Andrew Le <s3820098@rmit.edu.vn>
2022-02-24 08:45:18 +0700

    1. Populate application properties + create Student entity

---
+++ src/main/java/com/drewindeed/spring/data/jpa/tutorial/entity/Student.java
@@ -0,0 +1,24 @@
+package com.drewindeed.spring.data.jpa.tutorial.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+    @Id
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
+}
